b9b3065a57095b5facbc37674cad52f3ced25d03
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2024-03-13 05:00:00 +0100

    Update test.R

--- R/test.R
+++ R/test.R
@@ -20,8 +20,8 @@ my_example_function <- function(ARG_VALUE_1, ARG_VALUE_2) {
     on.exit(basiliskStop(proc))
 
     some_useful_thing <- basiliskRun(proc, fun=function(arg1, arg2) {
-        mod <- reticulate::import("scikit-learn")
-        output <- mod$some_calculation(arg1, arg2)
+        mod <- reticulate::import("torch")
+        output <- 3
 
         # The return value MUST be a pure R object, i.e., no reticulate
         # Python objects, no pointers to shared memory. 
