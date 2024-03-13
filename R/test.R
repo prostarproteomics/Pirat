7aa861e6c159b5f18db68bc56235e0ec00cc9317
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2024-03-13 04:55:49 +0100

    Update test.R

--- R/test.R
+++ R/test.R
@@ -1,5 +1,22 @@
+source_own_pyScripts <- function(){
+    
+    tryCatch({
+      dir.backup <- getwd()
+      setwd(system.file(".", package="Pirat"))
+      custom_scripts <- c("LBFGS.py", "llk_maximize.py")
+      for (i in custom_scripts)
+        reticulate::source_python(system.file("python", i, package = "Pirat"))
+      setwd(dir.backup)
+      },
+      warning = function(w) w,
+      error = function(e) e
+      )
+}
+
+
 my_example_function <- function(ARG_VALUE_1, ARG_VALUE_2) { 
     proc <- basiliskStart(my_env)
+    source_own_pyScripts()
     on.exit(basiliskStop(proc))
 
     some_useful_thing <- basiliskRun(proc, fun=function(arg1, arg2) {
